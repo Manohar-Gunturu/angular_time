9d988a7e3eba9d439ecf76c360bce78319383f3f
Manohar gunturu <gunturumanohar2@gmail.com>
2016-11-07 09:08:45 +0530

    Update angular_time.js

--- angular_time.js
+++ angular_time.js
@@ -8,7 +8,7 @@
   var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-  angular.module('ds.clock', [])
+  angular.module('rsclock', [])
     .directive('dsWidgetClock', ['$interval', '$filter',
       function($interval, $filter) {
         return clock($interval, $filter);
@@ -24,8 +24,6 @@
         showDigital: '=showDigital',
         showAnalog: '=showAnalog',
         startTime: '=startTime',
-        theme: '=theme',
-        majorsTotal: '=majorsTotal'
       },
       template: '<div class="widget-clock" ng-class="themeClass" ng-if="date"><div class="digital" ng-if="digital"><span class="time"><span class="hours">{{digital}}</span></span></div><div class="analog" ng-if="analog"><div class="square"><svg viewBox="0 0 100 100"><g transform="translate(50,50)"><circle class="clock-face" r="48" /><line ng-repeat="minor in minors track by $index" class="minor" y1="42" y2="45" ng-attr-transform="rotate({{360 * $index / minors.length}})" /><line ng-repeat="major in majors track by $index" class="major" y1="35" y2="46" ng-attr-transform="rotate({{360 * $index / majors.length}})" /><line class="hour" y1="2" y2="-20" ng-attr-transform="rotate({{30 * date.hrs + date.mins / 2}})" /><line class="minute" y1="4" y2="-30" ng-attr-transform="rotate({{6 * date.mins + date.secs / 10}})" /><g ng-attr-transform="rotate({{6 * date.secs}})"><line class="second" y1="10" y2="-38" /><line class="second-counterweight" y1="10" y2="2" /></g></g></svg></div></div><div ng-if="gmtInfo" class="gmt-info">{{gmtInfo}}</div></div>',
       link: function(scope, element, attrs) {
